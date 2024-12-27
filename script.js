73d1536ffe316a025bbdd30c1fc84c384623d3e8
brajagopaldas63 <brajagopaldas245@gmail.com>
2024-12-27 16:12:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function plan(){
+    let img=document.getElementById("myimage")
+    img.src="plan.png"
+    
+}
+
+function record(){
+    let img=document.getElementById("myimage")
+    img.src="record.png"
+
+}
+function relation(){
+    let rel=document.getElementById("myimage")
+    rel.src="relation.png"
+}
+function goal1(){
+    let g1=document.getElementById("goals_image")
+    g1.src="https://cms-images.udemycdn.com/96883mtakkm8/4kbyXne3Slx9Sfz4nTBqdf/8ac2b75db1a118f15e2fb5dfe2bb4567/desktop-hands-on-learning-2x.png"
+
+}
+function goal2(){
+    let g2=document.getElementById("goals_image")
+    g2.src="https://cms-images.udemycdn.com/96883mtakkm8/GUVYFTj0uwEQuJha5j7TZ/133c991fb3b3f1f93a3e842f4baa7f44/desktop-certification-prep-2x.png"
+}
+function goal3(){
+    let g3=document.getElementById("goals_image")
+    g3.src="https://cms-images.udemycdn.com/96883mtakkm8/6q4N9BvIQusFoheoALJhGj/678c1a0bb6c2a22d95461d409492231e/desktop-insights-and-analytics-2x.png"
+}
+function goal4(){
+    let g4=document.getElementById("goals_image")
+    g4.src="https://cms-images.udemycdn.com/96883mtakkm8/385IhnON960Wvz50ooWIN3/d4e6738c97769258d387b3d609edaad4/desktop-customizable-2x.png"
+}
+function plans_pricing(){
+    window.location="plans.html"
+}
+function ubussiness(){
+    window.location="bussiness.html"
+}
+function tech(){
+    window.location="tech.html"
+}
+function cart(){
+    window.location="cart.html"
+}
+function keepshoping(){
+    window.location="index.html"
+}
+function login(){
+    window.location="login.html"
+}
+function signup(){
+    window.location="signup.html"
+}
